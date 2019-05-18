91aa2c5213ac60dd83adeff5585123d9feee7a70
Carter Carlson <carlfarterson@gmail.com>
2019-05-18 00:06:24 -0400

    Basically finished.  Still need to extract email

--- chrome.py
+++ chrome.py
@@ -16,24 +16,17 @@ class Chrome:
         self._connect()
 
     def _connect(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
         self.web = webdriver.Chrome(os.getcwd() + '/chromedriver')
+        # self.web = webdriver.Chrome(os.getcwd() + '/chromedriver', options=options)
         self._login()
 
     def _login(self):
-        self.get('https://linkedin.com/login')
+        self.get_url('https://linkedin.com/login')
         self.send_keys('input[@id="username"]', self.username)
         self.send_keys('input[@id="password"]', self.password)
         self.click('button[@aria-label="Sign in"]')
-
-    def scroll_to_bottom(self):
-        
-        pass
-
-
-    def wait_for_element(self, element):
-        for interval in range(0, 5, load_interval):
-            time.sleep(interval)
-            results = self.web.find_elements_by_xpath('//' + element)
 
     def execute_js(self, string):
         self.web.execute_script(string)
@@ -42,27 +35,26 @@ class Chrome:
         self.web.close()
         self._connect()
 
-    def load_element(self, element):
-        return self.wait_for_element(element)
+    def wait_for_element(self, element):
+        for i in range(100):
+            time.sleep(.1)
+            if self.load(element) is not None:
+                return
 
-
-    def get(self, url):
-        time.sleep(0.1)
+    def get_url(self, url):
         self.web.get(url)
 
-    def load(self, element=None):
+    def load_element(self, element):
         results = self.web.find_elements_by_xpath('//' + element)
         if len(results) == 1:
             return results[0]
         else:
             return results
-        time.sleep(0.2)
-        return self.load(element)
 
     def send_keys(self, element, string):
-        textbox = self.load(element)
+        textbox = self.load_element(element)
         textbox.send_keys(string)
 
     def click(self, element):
-        clickable_element = self.load(element)
+        clickable_element = self.load_element(element)
         clickable_element.click()
--- linkedin.py
+++ linkedin.py
@@ -5,30 +5,34 @@ import time
 
 
 chrome = chrome.Chrome()
-chrome.get('https://linkedin.com/mynetwork/invite-connect/connections/')
+chrome.get_url('https://linkedin.com/mynetwork/invite-connect/connections/')
 
-
-chrome.scroll_down()
 
 while True:
+    connections = chrome.load_element('section/ul/li/div')
     time.sleep(1)
-    connections = chrome.load()
     chrome.execute_js('window.scrollBy(0,4000)')
-    if connections == chrome.load('section/ul/li/div'):
+    if connections == chrome.load_element('section/ul/li/div'):
         break
 
 
 mynetwork = []
 
+
+
 for connection in connections:
+    # connection = connections[0]
     row = connection.text.split('\n')
     name = row[0]
-    occupation = row[row.index("Member’s occupation") + 1]
+    for i in range(len(row)):
+        if 'occupation' in row[i]:
+            occupation = row[i+1]
     try:
         title, company = occupation.split(' at ')
     except:
         title = None
         company = occupation
+
     profile_link = connection.find_element_by_xpath('a').get_attribute('href')
     mynetwork.append({
         'Name': name,
@@ -40,12 +44,12 @@ for connection in connections:
 # NOTE: do I need to use this commented-out code in the future?
 # pd.DataFrame(mynetwork).to_csv('data.csv', index=False)
 for person in mynetwork:
-    if mynetwork.index(person) % 100 == 0:
-        chrome.refresh()
-    chrome.get(person['Profile Link'] + 'detail/contact-info')
-    location = chrome.load('div[@id="profile-wrapper"]/div[1]/div[1]/div[1]/div[2]/section/div[3]/div[1]/h3')
+    # if mynetwork.index(person) % 100 == 0:
+    #     chrome.refresh()
+    chrome.get_url(person['Profile Link'] + 'detail/contact-info')
+    location = chrome.load_element('div[@id="profile-wrapper"]/div[1]/div[1]/div[1]/div[2]/section/div[3]/div[1]/h3')
     person['Location'] = location.text
-    contact_info = chrome.load('artdeco-modal-content/section/div/div[1]/div/section')[1:]
+    contact_info = chrome.load_element('artdeco-modal-content/section/div/div[1]/div/section')[1:]
     for section in contact_info:
         description = section.text.split('\n')
         title = description.pop(0)
@@ -53,4 +57,5 @@ for person in mynetwork:
 
 
 chrome.web.close()
+# Company, Name, Title
 pd.DataFrame(mynetwork).to_csv('data2.csv', index=False)
